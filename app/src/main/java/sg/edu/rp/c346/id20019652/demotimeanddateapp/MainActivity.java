a00c077623b19c2b9f77bb55547540e00e91eee9
Liew Xi Xin <20019652@myrp.edu.sg>
2021-05-14 11:23:29 +0800

    first phase section A (modify date and time with spinner)

--- app/src/main/java/sg/edu/rp/c346/id20019652/demotimeanddateapp/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id20019652/demotimeanddateapp/MainActivity.java
@@ -3,12 +3,38 @@ package sg.edu.rp.c346.id20019652.demotimeanddateapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 public class MainActivity extends AppCompatActivity {
+
+    DatePicker dp;
+    TimePicker tp;
+    Button btnDisplayDate;
+    Button getBtnDisplayTime;
+    TextView tvDisplay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+        dp = findViewById(R.id.datePicker);
+        tp = findViewById(R.id.timePicker);
+        btnDisplayDate = findViewById(R.id.buttonDisplayDate);
+        getBtnDisplayTime = findViewById(R.id.buttonDisplayTime);
+        tvDisplay = findViewById(R.id.textViewDisplay);
+
+        getBtnDisplayTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String strResponse = getBtnDisplayTime.getText().toString();
+
+                tvDisplay.setText(strResponse);
+            }
+
+
+            }
 }
